86f5b8a9606b3dfacbc9a24599c9e009798619b7
unknown <emmanuelpeculiar645@gmail.com>
2025-05-03 22:53:27 +0100

    new commit

--- Kolo-IQ-web/src/App.tsx
+++ Kolo-IQ-web/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    web version of kolo-IQ
       </p>
     </>
   )
